7070df648159c2ceb1b672563360e9a3782df4f9
jheeettfire <jhettvivares@yahoo.com>
2021-02-10 17:18:20 +1100

    Add files via upload

---
+++ robco_pc/robco_pc/robco_pc/clear_screen.h
@@ -0,0 +1,11 @@
+//This is an include file for the RobCo PC
+//The file contains the function that allows to clear the command console to print
+//a new set of text
+
+//This is the pre determined command to clear the console screen
+#ifndef CLEAR_SCREEN_INCLUDED
+#define CLEAR_SCREEN_INCLUDED
+
+void ClearScreen();
+
+#endif CLEAR_SCREEN_INCLUDED
